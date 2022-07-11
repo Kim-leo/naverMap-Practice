4df9aa0b86f34fbd85c1683f17c9c4cdf5e4edd8
Kim - Leo <ochell2@naver.com>
2022-07-12 02:04:28 +0900

    mapView camera delegate 위임

--- naverMap/ViewController.swift
+++ naverMap/ViewController.swift
@@ -8,13 +8,23 @@
 import UIKit
 import NMapsMap
 
-class ViewController: UIViewController {
+
+
+class ViewController: UIViewController, NMFMapViewCameraDelegate {
+    
+    let CurrentCoord = NMGLatLng(lat: 37.5670135, lng: 126.9783740)
 
     override func viewDidLoad() {
         super.viewDidLoad()
         
         let mapView = NMFMapView(frame: view.frame)
         view.addSubview(mapView)
+        
+        mapView.addCameraDelegate(delegate: self)
+        
+        
+        
+        
     }
 
 
